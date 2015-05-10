f9b1ca6151c21041d46b67f64ca666913911c688
Andrew Kravchuk <awkravchuk@gmail.com>
2015-05-10 13:27:00 +0300

    Added exercise 1.3 solution

---
+++ 1/3.scm
@@ -0,0 +1,14 @@
+#lang planet neil/sicp
+
+(define (square x) (* x x))
+
+(define (most-squares a b c) 
+  (cond ((and (>= a c) (>= b c)) (+ (square a) (square b)))
+        ((and (>= b a) (>= c a)) (+ (square b) (square c)))
+        ((and (>= a b) (>= c b)) (+ (square a) (square c))) ))
+
+(most-squares 1 2 3)
+(most-squares 2 1 3)
+(most-squares 2 3 1)
+(most-squares 3 1 2)
+(most-squares 3 2 1)
