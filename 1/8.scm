9ab9afdd8cca902941fb3c60f8d0edf50ada9c1b
Andrew Kravchuk <awkravchuk@gmail.com>
2015-05-10 14:01:26 +0300

    Added exercise 1.8 solution

---
+++ 1/8.scm
@@ -0,0 +1,20 @@
+#lang planet neil/sicp
+
+(define (cbrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cbrt-iter (improve guess x)
+                 x)))
+
+(define (improve y x)
+  (/ (+ (/ x (square y)) (* 2 y)) 3))
+
+(define (square x) (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (* guess guess guess) x)) 0.001))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 x))
+
+(cbrt 8)
