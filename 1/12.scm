eb5833637cbc7f6d6c8ace3e4fbbe471be23abd1
Andrew Kravchuk <awkravchuk@gmail.com>
2015-05-14 12:50:23 +0300

    Added exercise 1.12 solution

---
+++ 1/12.scm
@@ -0,0 +1,22 @@
+#lang planet neil/sicp
+
+(define (pascal-triangle row col)
+  (cond ((= row 1) 1)
+        ((= row 2) 1)
+        ((= col 1) 1)
+        ((= col row) 1)
+        (else (+ 
+                 (pascal-triangle (- row 1) (- col 1))
+                 (pascal-triangle (- row 1) col) 
+               )
+        )
+  )
+)
+
+(pascal-triangle 3 2)
+
+(pascal-triangle 5 1)
+(pascal-triangle 5 2)
+(pascal-triangle 5 3)
+(pascal-triangle 5 4)
+(pascal-triangle 5 5)
