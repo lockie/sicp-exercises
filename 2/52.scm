784f5287c628d38cf0bde9034b699703b8eb87a2
Andrew Kravchuk <awkravchuk@gmail.com>
2017-11-01 18:25:44 +0300

    Added excercise 2.52 solution

---
+++ 2/52.scm
@@ -0,0 +1,56 @@
+#lang sicp
+(#%require sicp-pict)
+
+;; boilerplate
+(define wave
+  (segments->painter
+   (list
+    (make-segment (make-vect 0.45 0.80) (make-vect 0.50 0.75))
+    (make-segment (make-vect 0.50 0.75) (make-vect 0.55 0.80))  ;; a part
+    (make-segment (make-vect 0.20 0.00) (make-vect 0.35 0.50))
+    (make-segment (make-vect 0.35 0.50) (make-vect 0.30 0.60))
+    (make-segment (make-vect 0.30 0.60) (make-vect 0.15 0.45))
+    (make-segment (make-vect 0.15 0.45) (make-vect 0.00 0.60))
+    (make-segment (make-vect 0.00 0.80) (make-vect 0.15 0.65))
+    (make-segment (make-vect 0.15 0.65) (make-vect 0.30 0.70))
+    (make-segment (make-vect 0.30 0.70) (make-vect 0.40 0.70))
+    (make-segment (make-vect 0.40 0.70) (make-vect 0.35 0.85))
+    (make-segment (make-vect 0.35 0.85) (make-vect 0.40 1.00))
+    (make-segment (make-vect 0.60 1.00) (make-vect 0.65 0.85))
+    (make-segment (make-vect 0.65 0.85) (make-vect 0.60 0.70))
+    (make-segment (make-vect 0.60 0.70) (make-vect 0.75 0.70))
+    (make-segment (make-vect 0.75 0.70) (make-vect 1.00 0.40))
+    (make-segment (make-vect 1.00 0.20) (make-vect 0.60 0.48))
+    (make-segment (make-vect 0.60 0.48) (make-vect 0.80 0.00))
+    (make-segment (make-vect 0.40 0.00) (make-vect 0.50 0.30))
+    (make-segment (make-vect 0.50 0.30) (make-vect 0.60 0.00)))))
+
+(define (up-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (up-split painter (- n 1))))
+            (below painter (beside smaller smaller)))))
+
+(define (right-split painter n)
+    (if (= n 0)
+        painter
+        (let ((smaller (right-split painter (- n 1))))
+            (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+    (if (= n 0)
+        painter
+        (let ((up (up-split painter (- n 1)))
+              (right (right-split painter (- n 1))))
+            (let ((top-left up)
+                  (bottom-right right)
+                  (corner (corner-split painter (- n 1))))
+                (beside (below painter top-left)
+                        (below bottom-right corner))))))
+
+(define (square-limit painter n)
+    (let ((quarter (corner-split painter n)))
+        (let ((half (beside (flip-horiz quarter) einstein)))
+            (below (flip-vert half) half))))
+
+(paint (square-limit wave 2))
