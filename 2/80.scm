3c5f5e800275cd22930f9cde29d0a3e1b302d9d6
Andrew Kravchuk <awkravchuk@gmail.com>
2017-11-07 17:53:08 +0300

    Added excercise 2.80 solution

---
+++ 2/80.scm
@@ -0,0 +1,227 @@
+#lang sicp
+
+
+;; get/put boilerplate
+
+(#%require (only racket/base make-hash))
+(#%require (only racket/base hash-set!))
+(#%require (only racket/base hash-ref))
+
+(define *op-table* (make-hash))
+
+(define (put op type proc)
+    (hash-set! *op-table* (list op type) proc))
+
+(define (get op type)
+    (hash-ref *op-table* (list op type) '()))
+
+
+;; some required boilerplate
+
+(define (square x) (* x x))
+
+(define (attach-tag type-tag contents)
+    (cons type-tag contents))
+
+(define (type-tag datum)
+    (if (pair? datum)
+        (car datum)
+        (error "Некорректные помеченные данные -- TYPE-TAG" datum)))
+
+(define (contents datum)
+    (if (pair? datum)
+        (cdr datum)
+        (error "Некорректные помеченные данные -- CONTENTS" datum)))
+
+(define (apply-generic op . args)
+    (let ((type-tags (map type-tag args)))
+        (let ((proc (get op type-tags)))
+            (if proc
+                (apply proc (map contents args))
+                (error "Нет метода для этих типов -- APPLY-GENERIC"
+                       (list op type-tags))))))
+
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+(define (=zero? x) (apply-generic '=zero? x))
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+
+
+;; scheme number boilerplate
+
+(define (install-scheme-number-package)
+    (define (tag x)
+        (attach-tag 'scheme-number x))
+    (put 'add '(scheme-number scheme-number)
+         (lambda (x y) (tag (+ x y))))
+    (put 'sub '(scheme-number scheme-number)
+         (lambda (x y) (tag (- x y))))
+    (put 'mul '(scheme-number scheme-number)
+         (lambda (x y) (tag (* x y))))
+    (put 'div '(scheme-number scheme-number)
+         (lambda (x y) (tag (/ x y))))
+    (put '=zero? '(scheme-number)
+         (lambda (x) (= x 0)))
+    (put 'make 'scheme-number
+         (lambda (x) (tag x))))
+
+(define (make-scheme-number n)
+    ((get 'make 'scheme-number) n))
+
+
+;;  rational boilerplate
+
+(define (install-rational-package)
+    ;; внутренние процедуры
+    (define (numer x) (car x))
+    (define (denom x) (cdr x))
+    (define (make-rat n d)
+        (let ((g (gcd n d)))
+            (cons (/ n g) (/ d g))))
+    (define (add-rat x y)
+        (make-rat (+ (* (numer x) (denom y))
+                     (* (numer y) (denom x)))
+                  (* (denom x) (denom y))))
+    (define (sub-rat x y)
+        (make-rat (- (* (numer x) (denom y))
+                     (* (numer y) (denom x)))
+                  (* (denom x) (denom y))))
+    (define (mul-rat x y)
+        (make-rat (* (numer x) (numer y))
+                  (* (denom x) (denom y))))
+    (define (div-rat x y)
+        (make-rat (* (numer x) (denom y))
+                  (* (denom x) (numer y))))
+    ;; интерфейс к остальной системе
+    (define (tag x) (attach-tag 'rational x))
+    (put 'add '(rational rational)
+         (lambda (x y) (tag (add-rat x y))))
+    (put 'sub '(rational rational)
+         (lambda (x y) (tag (sub-rat x y))))
+    (put 'mul '(rational rational)
+         (lambda (x y) (tag (mul-rat x y))))
+    (put 'div '(rational rational)
+         (lambda (x y) (tag (div-rat x y))))
+    (put '=zero? '(rational)
+         (lambda (x) (= (numer x) 0)))
+    (put 'make 'rational
+         (lambda (n d) (tag (make-rat n d)))))
+
+(define (make-rational n d)
+    ((get 'make 'rational) n d))
+
+
+;; complex boilerplate
+
+(define (install-rectangular-package)
+    ;; внутренние процедуры
+    (define (real-part z) (car z))
+    (define (imag-part z) (cdr z))
+    (define (make-from-real-imag x y) (cons x y))
+    (define (magnitude z)
+        (sqrt (+ (square (real-part z))
+                 (square (imag-part z)))))
+    (define (angle z)
+        (atan (imag-part z) (real-part z)))
+    (define (make-from-mag-ang r a)
+        (cons (* r (cos a)) (* r (sin a))))
+    ;; интерфейс к остальной системе
+    (define (tag x) (attach-tag 'rectangular x))
+    (put 'real-part '(rectangular) real-part)
+    (put 'imag-part '(rectangular) imag-part)
+    (put 'magnitude '(rectangular) magnitude)
+    (put 'angle '(rectangular) angle)
+    (put 'make-from-real-imag 'rectangular
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'rectangular
+         (lambda (r a) (tag (make-from-mag-ang r a)))))
+
+(define (install-polar-package)
+    ;; внутренние процедуры
+    (define (magnitude z) (car z))
+    (define (angle z) (cdr z))
+    (define (make-from-mag-ang r a) (cons r a))
+    (define (real-part z)
+        (* (magnitude z) (cos (angle z))))
+    (define (imag-part z)
+        (* (magnitude z) (sin (angle z))))
+    (define (make-from-real-imag x y)
+        (cons (sqrt (+ (square x) (square y)))
+              (atan y x)))
+    ;; интерфейс к остальной системе
+    (define (tag x) (attach-tag 'polar x))
+    (put 'real-part '(polar) real-part)
+    (put 'imag-part '(polar) imag-part)
+    (put 'magnitude '(polar) magnitude)
+    (put 'angle '(polar) angle)
+    (put 'make-from-real-imag 'polar
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'polar
+         (lambda (r a) (tag (make-from-mag-ang r a)))))
+
+(define (install-complex-package)
+    ;; процедуры, импортируемые из декартова
+    ;; и полярного пакетов
+    (define (make-from-real-imag x y)
+        ((get 'make-from-real-imag 'rectangular) x y))
+    (define (make-from-mag-ang r a)
+        ((get 'make-from-mag-ang 'polar) r a))
+    ;; внутренние процедуры
+    (define (add-complex z1 z2)
+        (make-from-real-imag (+ (real-part z1) (real-part z2))
+                             (+ (imag-part z1) (imag-part z2))))
+    (define (sub-complex z1 z2)
+        (make-from-real-imag (- (real-part z1) (real-part z2))
+                             (- (imag-part z1) (imag-part z2))))
+    (define (mul-complex z1 z2)
+        (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                           (+ (angle z1) (angle z2))))
+    (define (div-complex z1 z2)
+        (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                           (- (angle z1) (angle z2))))
+    ;; интерфейс к остальной системе
+    (define (tag z) (attach-tag 'complex z))
+    (put 'add '(complex complex)
+         (lambda (z1 z2) (tag (add-complex z1 z2))))
+    (put 'sub '(complex complex)
+         (lambda (z1 z2) (tag (sub-complex z1 z2))))
+    (put 'mul '(complex complex)
+         (lambda (z1 z2) (tag (mul-complex z1 z2))))
+    (put 'div '(complex complex)
+         (lambda (z1 z2) (tag (div-complex z1 z2))))
+    (put '=zero? '(complex)
+         (lambda (z) (= (magnitude z) 0)))
+    (put 'make-from-real-imag 'complex
+         (lambda (x y) (tag (make-from-real-imag x y))))
+    (put 'make-from-mag-ang 'complex
+         (lambda (r a) (tag (make-from-mag-ang r a)))))
+
+(define (make-complex-from-real-imag x y)
+    ((get 'make-from-real-imag 'complex) x y))
+
+(define (make-complex-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'complex) r a))
+
+
+;; actual code
+
+
+(install-scheme-number-package)
+(install-rational-package)
+(install-rectangular-package)
+(install-polar-package)
+(install-complex-package)
+
+(=zero? (make-scheme-number 0))
+(=zero? (make-rational 0 2))
+(=zero? (make-complex-from-mag-ang 0 -1))
+
+(=zero? (make-scheme-number 1))
+(=zero? (make-rational 1 0))
+(=zero? (make-complex-from-real-imag 1 -1))
