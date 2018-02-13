41be73f50d9db442ba9cbf42f91cc85c617859e8
Andrew Kravchuk <awkravchuk@gmail.com>
2018-02-13 22:44:17 +0300

    Added excercise 3.56 solution

---
+++ 3/56.scm
@@ -0,0 +1,90 @@
+#lang sicp
+
+
+;; nasty boilerplate
+(#%require racket/stream)
+(define-syntax cons-stream
+    (syntax-rules ()
+        ((_ a b) (stream-cons a b))))
+(define stream-car stream-first)
+(define stream-cdr stream-rest)
+(define stream-null? stream-empty?)
+(define the-empty-stream empty-stream)
+
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+        (begin (proc (stream-car s))
+               (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream) the-empty-stream)
+          ((pred (stream-car stream))
+           (cons-stream (stream-car stream)
+                        (stream-filter pred
+                                       (stream-cdr stream))))
+          (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        the-empty-stream
+        (cons-stream
+         low
+         (stream-enumerate-interval (+ low 1) high))))
+
+(define (display-stream s)
+    (define (display-line x)
+        (newline)
+        (display x))
+    (stream-for-each display-line s))
+
+(define (stream-map proc . argstreams)
+    (if (stream-null? (car argstreams))
+        the-empty-stream
+        (cons-stream
+         (apply proc (map stream-car argstreams))
+         (apply stream-map
+                (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+    (stream-map + s1 s2))
+
+(define (scale-stream stream factor)
+    (stream-map (lambda (x) (* x factor)) stream))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+
+;; actual code
+
+(define (merge s1 s2)
+    (cond ((stream-null? s1) s2)
+          ((stream-null? s2) s1)
+          (else
+           (let ((s1car (stream-car s1))
+                 (s2car (stream-car s2)))
+               (cond ((< s1car s2car)
+                      (cons-stream s1car (merge (stream-cdr s1) s2)))
+                     ((> s1car s2car)
+                      (cons-stream s2car (merge s1 (stream-cdr s2))))
+                     (else
+                      (cons-stream s1car
+                                   (merge (stream-cdr s1)
+                                          (stream-cdr s2)))))))))
+
+(define S (cons-stream 1 (merge (merge (scale-stream S 2) (scale-stream S 3))
+                                (scale-stream S 5))))
+
+(stream-ref S 0)
+(stream-ref S 1)
+(stream-ref S 2)
+(stream-ref S 3)
+(stream-ref S 4)
+(stream-ref S 5)
+(stream-ref S 6)
+(stream-ref S 7)
+(stream-ref S 8)
+(stream-ref S 9)
+(stream-ref S 10)
