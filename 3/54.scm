61a5b5671eaa6bf938581dba12fd1f542edba81a
Andrew Kravchuk <awkravchuk@gmail.com>
2018-02-13 21:35:22 +0300

    Added excercise 3.54 solution

---
+++ 3/54.scm
@@ -0,0 +1,74 @@
+#lang sicp
+
+
+;; nasty boilerplate
+(#%require racket/stream)
+(define-syntax cons-stream
+    (syntax-rules ()
+        ((_ a b) (stream-cons a b))))
+(define stream-car stream-first)
+(define stream-cdr stream-rest)
+(define stream-null? stream-empty?)
+(define the-empty-stream empty-stream)
+
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+        (begin (proc (stream-car s))
+               (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream) the-empty-stream)
+          ((pred (stream-car stream))
+           (cons-stream (stream-car stream)
+                        (stream-filter pred
+                                       (stream-cdr stream))))
+          (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        the-empty-stream
+        (cons-stream
+         low
+         (stream-enumerate-interval (+ low 1) high))))
+
+(define (display-stream s)
+    (define (display-line x)
+        (newline)
+        (display x))
+    (stream-for-each display-line s))
+
+(define (stream-map proc . argstreams)
+    (if (stream-null? (car argstreams))
+        the-empty-stream
+        (cons-stream
+         (apply proc (map stream-car argstreams))
+         (apply stream-map
+                (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+    (stream-map + s1 s2))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+
+;; actual code
+
+(define (mul-streams s1 s2)
+    (stream-map * s1 s2))
+
+(define factorials (cons-stream 1 (mul-streams factorials (stream-cdr integers))))
+
+(stream-ref factorials 0)
+(stream-ref factorials 1)
+(stream-ref factorials 2)
+(stream-ref factorials 3)
+(stream-ref factorials 4)
+(stream-ref factorials 5)
+(stream-ref factorials 6)
+(stream-ref factorials 7)
+(stream-ref factorials 8)
+(stream-ref factorials 9)
+(stream-ref factorials 10)
