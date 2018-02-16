a08d8498abc1251c731c1c6f9ebfc7d6de900c98
Andrew Kravchuk <awkravchuk@gmail.com>
2018-02-16 21:40:08 +0300

    Added excercise 3.68 solution

---
+++ 3/68.scm
@@ -0,0 +1,86 @@
+#lang sicp
+
+
+;; nasty boilerplate
+(#%require racket/stream)
+(define-syntax cons-stream
+    (syntax-rules ()
+        ((_ a b) (stream-cons a b))))
+(define stream-car stream-first)
+(define stream-cdr stream-rest)
+(define stream-null? stream-empty?)
+(define the-empty-stream empty-stream)
+
+(define (stream-for-each proc s)
+    (if (stream-null? s)
+        'done
+        (begin (proc (stream-car s))
+               (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-filter pred stream)
+    (cond ((stream-null? stream) the-empty-stream)
+          ((pred (stream-car stream))
+           (cons-stream (stream-car stream)
+                        (stream-filter pred
+                                       (stream-cdr stream))))
+          (else (stream-filter pred (stream-cdr stream)))))
+
+(define (stream-enumerate-interval low high)
+    (if (> low high)
+        the-empty-stream
+        (cons-stream
+         low
+         (stream-enumerate-interval (+ low 1) high))))
+
+(define (display-line x)
+    (newline)
+    (display x))
+
+(define (display-stream s)
+    (stream-for-each display-line s))
+
+(define (display-stream-partial s N)
+    (define (display-stream-partial-iter s n)
+        (cond ((< n N)
+               (begin
+                   (display-line (stream-car s))
+                   (display-stream-partial-iter (stream-cdr s) (+ n 1))))
+               (else 'done)))
+    (display-stream-partial-iter s 0))
+
+(define (stream-map proc . argstreams)
+    (if (stream-null? (car argstreams))
+        the-empty-stream
+        (cons-stream
+         (apply proc (map stream-car argstreams))
+         (apply stream-map
+                (cons proc (map stream-cdr argstreams))))))
+
+(define (add-streams s1 s2)
+    (stream-map + s1 s2))
+
+(define (scale-stream stream factor)
+    (stream-map (lambda (x) (* x factor)) stream))
+
+(define ones (cons-stream 1 ones))
+
+(define integers (cons-stream 1 (add-streams ones integers)))
+
+
+;; actual code
+
+(define (interleave s1 s2)
+    (if (stream-null? s1)
+        s2
+        (cons-stream (stream-car s1)
+                     (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+    (interleave
+     (stream-map (lambda (x) (list (stream-car s) x))
+                 t)
+     (pairs (stream-cdr s) (stream-cdr t))))
+
+(display "Here goes infinite recursion!")
+(newline)
+(display-stream-partial (pairs integers integers) 100)
