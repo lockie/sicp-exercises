a4d8605a68589c93cde68a784b51ff22611dc70d
Andrew Kravchuk <awkravchuk@gmail.com>
2018-06-06 09:34:35 +0300

    Added excercise 4.61 solution

---
+++ 4/61.scm
@@ -0,0 +1,7 @@
+(assert! (rule (?x next-to ?y in (?x ?y . ?u))))
+(assert! (rule (?x next-to ?y in (?v . ?z))
+               (?x next-to ?y in ?z)))
+
+(?x next-to ?y in (1 (2 3) 4))
+
+(?x next-to 1 in (2 1 3 1))
